97a3d7fa2054b42dae0fbc1fc0000f2c4ab9c290
logan <loanmorr@ucsc.edu>
2020-08-08 19:10:26 -0700

    Starting the radau code

---
+++ include/stiff/radau/cache.hpp
@@ -0,0 +1,10 @@
+//
+// Created by logan on 8/8/20.
+//
+
+#ifndef STIFF_RADAU_CACHE_HPP
+#define STIFF_RADAU_CACHE_HPP
+
+namespace stiff::radau {} // namespace stiff::radau
+
+#endif // STIFF_RADAU_CACHE_HPP
---
+++ test/test_radau.cpp
@@ -0,0 +1,5 @@
+//
+// Created by logan on 8/8/20.
+//
+
+#include <stiff/radau/cache.hpp>
